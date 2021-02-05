356b818d0c6d37c1117190f666fff8f0c62dcf58
Claus Guttesen <kometen@gmail.com>
2021-02-05 20:59:47 +0100

    Generate random data using Faker

---
+++ gen_data.py
@@ -0,0 +1,21 @@
+import uuid
+from faker import Faker
+fake = Faker()
+
+for _ in range(10000000):
+    print(
+        str(fake.date_time_between(start_date='-30y', end_date='+30y')) +
+        ";" +
+         fake.firefox() +
+        ";" +
+        fake.postcode() +
+        ";" +
+        fake.credit_card_provider() +
+        ";" +
+        fake.cryptocurrency_code() +
+        ";" +
+        str(fake.location_on_land()).replace("'", "").replace("(", "").replace(")", "").replace(", ", ";") +
+        ";" +
+        str(uuid.uuid4())
+    )
+# datetime.datetime(2007, 2, 28, 11, 28, 16)
